29ce018a07cd27f3c54200db3bdc5c6df5c55069
wowotech <wowotech@gmail.com>
2016-04-14 13:27:06 +0800

    Add copyright.

--- content/templates/default/footer.php
+++ content/templates/default/footer.php
@@ -7,7 +7,8 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 </div><!--end #content-->
 <div style="clear:both;"></div>
 <div id="footerbar">
-	Powered by <a href="http://www.emlog.net" title="采用emlog系统">emlog</a> 
+	Copyright @ 2013-2015 <a href="http://www.wowotech.net" title="wowotech">蜗窝科技</a> All rights reserved.
+	Powered by <a href="http://www.emlog.net" title="采用emlog系统">emlog</a>
 	<a href="http://www.miibeian.gov.cn" target="_blank"><?php echo $icp; ?></a> <?php echo $footer_info; ?>
 	<?php doAction('index_footer'); ?>
 </div><!--end #footerbar-->
